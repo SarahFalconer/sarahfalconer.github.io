5f863093264db41c34e002b31bfb75d2449a16b7
Shaun Smith <shaundsmith3@gmail.com>
2021-02-27 14:36:49 +0000

    Purpleised and tweaked the profile wording

--- src/parts/profile.jsx
+++ src/parts/profile.jsx
@@ -3,7 +3,7 @@ export default {
     title: "Profile",
     content: [
         "An enthusiastic and persistent worker in both individual and team-based tasks.",
-        "I thoroughly enjoy all aspects of research, data analysis, and presenting new results and techniques.",
+        "I thoroughly enjoy all aspects of research, data analysis, and personal development.",
         "I strive to perform well within groups and individually, and have excellent management and communication skills.",
     ].join(" "),
     icon: "usertie"
