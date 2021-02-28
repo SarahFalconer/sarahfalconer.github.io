0b8584748a6f6e296aeaa1a33288d4ee33202efb
Shaun Smith <shaundsmith3@gmail.com>
2021-02-28 14:50:28 +0000

    Updated descriptions of projects to be less technical and more personal

--- src/parts/projects.jsx
+++ src/parts/projects.jsx
@@ -7,24 +7,32 @@ export default {
     items: [
         {
             title: (<>Modelling, Materials, and Methods for Needle Insertion<br/> Investigation in Biomechanics</>),
+            authorityWebSite: "http://etheses.whiterose.ac.uk/26309/",
+            authority: "Ph.D Thesis on White Rose eTheses Online",
             rightSide: "2015-2019",
-            description: [
-                "An investigation into the current literature showed that existing skin tissue surrogates did not perform similarly to real skin tissue in vivo during needle insertions.",
-                "A new gel surrogate was developed which fractured in the same way as skin tissue, and it was subject to rigorous mechanical testing to assess its mechanical properties through tension and compression.",
-                "The components of the gel surrogate were modified and the mechanical properties were assessed at each concentration until the surrogate matched the published mechanical properties of skin tissue in vivo.\n",
-                "The newly developed surrogate was used in a variety of needle insertion experiments, where a robotically controlled needle was inserted into the surrogate.",
-                "The force response and needle deflection were recorded for a variety of needle types inserted at different speeds.",
-                "The results were compared to results in the literature and it was found that the new surrogate accurately replicates the response of needle insertions through skin tissue better than conventional surrogates.",
-                "The needle insertion experiments were recorded using a poleidoscope, which is a novel digital polariscope.",
-                "This optical analysis allowed for full-field analysis of each needle insertion, which revealed never-before-seen data on the causes of needle deflection."
-            ].join(" ")
+            description: (
+                <>
+                    The main aim of my Ph.D was to develop a new tissue surrogate that better replicated the properties
+                    of human soft tissue.
+                    Research into the current literature showed that existing skin tissue surrogates did not perform
+                    similarly to real skin tissue <em>in vivo</em> during needle insertions.<br/>
+                    I developed a new gel surrogate which performed the same as skin tissue, and I conducted rigorous
+                    mechanical testing to assess its mechanical properties.
+                    The newly developed surrogate was used in a variety of needle insertion experiments and results
+                    showed that the new surrogate better replicated the response of needle insertions through skin
+                    tissue than conventional surrogates.<br/>
+                    I used Microsoft Excel and specialised software to capture and process the vast quantities of data
+                    produced by my experiements.
+                </>
+            )
         },
         {
             title: "The Integrity of Chocolate Structures",
+            authority: "Master's Degree Thesis",
             rightSide: "2014-2015",
             description: [
                 "The fats in chocolate are polymorphic; they can crystallise into six different forms.",
-                "This project investigated the mechanical and subjective properties of the various crystalline forms.",
+                "In this project I investigated the mechanical and subjective properties of the various crystalline forms.",
                 "Different tempering methods were used to obtain a variety of crystal types.",
                 "The elastic moduli of the different crystal types were determined using mechanical tests; such as Charpy impact and three-point bending experiments.",
                 "A blind-folded perception test was also completed to assess which concentration of crystal type is more appealing."
@@ -32,6 +40,7 @@ export default {
         },
         {
             title: "Critical Care Chair",
+            authority: "Bachelor's Degree Project",
             rightSide: "2014",
             description: [
                 "Within a group of six students we designed a new critical care chair in conjunction with Knight Imaging and The John Radcliffe Hospital, Oxford.",
